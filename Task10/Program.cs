c6fb2e9dfc5c16cface9aa3b63ab52d53867a081
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-07-14 18:59:16 +0500

    Task10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите любое трёхзначное число: ");
+int number = int.Parse (Console.ReadLine());
+if (number>=100 && number<=999)
+{
+    number = number/10;
+    Console.WriteLine(number%10);
+}
+else Console.WriteLine("Вы ввели неверное число");
